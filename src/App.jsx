d97ebf1298c8ebf11d64fdc90bbaac0b737e8c69
Luan Alexandre <luanrb19@gmail.com>
2025-02-21 18:10:28 -0400

    Adiciona pagina inicial

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,14 @@
-import { useState } from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.min.js";
 import "./App.css";
-import Qrcode from "./Qrcode";
-import Color from "./components/color/Main";
+import Home from "./pages/Home";
 
 const App = () => {
-  const [url, setUrl] = useState("");
-  const [fgColor, setFgColor] = useState("#000000");
-  const [bgColor, setBgColor] = useState("#FFFFFF");
-
   return (
     <main className="d-flex justify-content-center align-items-center">
-      <Color
-        fgColor={fgColor}
-        setFgColor={setFgColor}
-        bgColor={bgColor}
-        setBgColor={setBgColor}
-      />
-      <Qrcode value={url} fgColor={fgColor} bgColor={bgColor} />
+      <section className="d-flex container">
+        <Home />
+      </section>
     </main>
   );
 };
