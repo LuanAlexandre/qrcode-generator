aa286dbd673aa9f33a6845624e49ad3d904d0ae9
Luan Alexandre <luanrb19@gmail.com>
2025-02-23 19:01:06 -0400

    Adiciona e configura campo de url

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,20 @@
+import { useState } from "react";
+import URL from "../components/url/Index";
 import QrCode from "../components/qrcode/Index";
 
 const Home = () => {
-  return <QrCode />;
+  const [data, setData] = useState("valerteatro.com.br");
+
+  return (
+    <>
+      <section>
+        <URL url={data} setUrl={setData} />
+      </section>
+      <section>
+        <QrCode data={data} />;
+      </section>
+    </>
+  );
 };
 
 export default Home;
